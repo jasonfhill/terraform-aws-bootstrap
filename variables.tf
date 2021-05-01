d4b98ab799a9e0d1b1ee248e8b8c8506517180f5
Jason Hill <jason@hill.io>
2021-05-01 10:52:56 -0400

    initial layout

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "account_alias" {
+  description = "The desired AWS account alias."
+  type        = string
+}
