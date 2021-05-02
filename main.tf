41066153481a7553ae5544074d9bb537d86efb33
Jason Hill <jason@hill.io>
2021-05-02 18:22:27 -0400

    removing alias

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 locals {
-  tags                        = merge(var.tags,{
+  tags = merge(var.tags,{
 
   })
 }
+
 resource "aws_iam_account_alias" "alias" {
   account_alias = var.namespace
 }
--- modules/state_bucket/main.tf
+++ modules/state_bucket/main.tf
@@ -1,4 +1,4 @@
-data "aws_iam_account_alias" "current" {}
+//data "aws_iam_account_alias" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "supplemental_policy" {
